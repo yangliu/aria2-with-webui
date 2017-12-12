c7ddac6f4b816928c56fc9866f4c7499db01fb20
Yang Liu <i@yangliu.name>
2017-12-12 09:29:00 +0800

    Fix bugs related user and group mapping

--- files/start.sh
+++ files/start.sh
@@ -12,9 +12,14 @@ USER_ID=${PUID:-9001}
 GROUP_ID=${PGID:-9001}
 
 echo "Add group \"${GROUP_NAME}\" with GID: ${GROUP_ID}"
-addgroup -g ${GROUP_ID} aria2 || export GROUP_NAME=`getent group | grep ${GROUP_ID} | cut -d: -f1` && echo "Now group name changed to ${GROUP_NAME}."
+addgroup -g ${GROUP_ID} aria2
+export GROUP_NAME=`getent group | grep ${GROUP_ID} | cut -d: -f1`
+echo "Group name: ${GROUP_NAME}."
+
 echo "Add user \"${USER_NAME}\" with UID: ${USER_ID}"
-adduser -D -H -u ${USER_ID} -G ${GROUP_NAME} ${USER_NAME} || export USER_NAME=`getent passwd | grep ${USER_ID}:${GROUP_ID} | cut -d: -f1` && echo "Now user name changed to ${USER_NAME}."
+adduser -D -H -s /bin/bash -u ${USER_ID} -G ${GROUP_NAME} ${USER_NAME}
+export USER_NAME=`getent passwd | grep ${USER_ID}:${GROUP_ID} | cut -d: -f1`
+echo "User name: ${USER_NAME}."
 
 chown -R ${USER_NAME}:${GROUP_NAME} /conf
 chown -R ${USER_NAME}:${GROUP_NAME} /conf-copy
